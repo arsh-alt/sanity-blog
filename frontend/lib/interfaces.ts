3c111572dc94c162fc682f5c736a3faf550958e7
Arshdeep Singh <73338971+arshdev16@users.noreply.github.com>
2022-09-21 20:47:10 +0530

    fetching blogs

---
+++ frontend/lib/interfaces.ts
@@ -0,0 +1,21 @@
+export interface AuthorModel{
+    name: string;
+    bio: string;
+    avatar: object;
+}
+
+export interface SlugModel{
+    current: string;
+    _type: string;
+}
+
+export interface BlogModel{
+    title: string;
+    metadesc: string;
+    content: Array<object>;
+    createdAt: string;
+    _id: string;
+    blogImage: object;
+    author: AuthorModel;
+    slug: SlugModel;
+} 
